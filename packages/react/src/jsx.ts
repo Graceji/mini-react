cfe58f58e39dd03262709bd5aeef03fb506bb7cf
Graceji <jina@af.local>
2025-11-02 22:17:45 +0800

    chore: 实现jsx打包

--- packages/react/src/jsx.ts
+++ packages/react/src/jsx.ts
@@ -65,5 +65,31 @@ export const jsx = (type: ElementType, config: any, ...maybeChildren: any) => {
 
 	return ReactElement(type, key, ref, props);
 };
+export const jsxDEV = (type: ElementType, config: any) => {
+	let key: Key = null;
+	const props: Props = {};
+	let ref: Ref = null;
+	for (const prop in config) {
+		const val = config[prop];
 
-export const jsxDev = jsx;
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = '' + val;
+			}
+			continue;
+		}
+
+		if (key === 'ref') {
+			if (val !== undefined) {
+				ref = val;
+			}
+			continue;
+		}
+
+		if ({}.hasOwnProperty.call(config, prop)) {
+			props[prop] = val;
+		}
+	}
+
+	return ReactElement(type, key, ref, props);
+};
